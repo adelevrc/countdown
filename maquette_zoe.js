9a2712bdde3399e2a0a8913e6e2d8c2f9bf5ae29
adelevrc <adele.vercaygne@gmail.com>
2020-11-02 15:26:44 +0100

    Update maquette_zoe.js

--- maquette_zoe.js
+++ maquette_zoe.js
@@ -10,8 +10,8 @@ let countDown = new Date('Mar 22, 2021 00:00:00').getTime(),
           distance = countDown - now;
 
       document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
     }, second)
